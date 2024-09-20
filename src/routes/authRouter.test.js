bf3b550c081613253553f84df266501e6be8e4a2
Hcran18 <hunter.crandall18@gmail.com>
2024-09-20 17:01:00 -0600

    test(add updateUser test)

--- src/routes/authRouter.test.js
+++ src/routes/authRouter.test.js
@@ -3,11 +3,19 @@ const app = require("../service");
 
 const testUser = { name: "pizza diner", email: "reg@test.com", password: "a" };
 let testUserAuthToken;
+let testUserId;
 
 beforeAll(async () => {
   testUser.email = Math.random().toString(36).substring(2, 12) + "@test.com";
   const registerRes = await request(app).post("/api/auth").send(testUser);
   testUserAuthToken = registerRes.body.token;
+  testUserId = registerRes.body.user.id;
+});
+
+test("register missing required data", async () => {
+  const res = await request(app).post("/api/auth").send({});
+  expect(res.status).toBe(400);
+  expect(res.body.message).toBe("name, email, and password are required");
 });
 
 test("login", async () => {
@@ -27,8 +35,14 @@ test("logout", async () => {
   expect(logoutRes.body.message).toBe("logout successful");
 });
 
-test("register missing required data", async () => {
-  const res = await request(app).post("/api/auth").send({});
-  expect(res.status).toBe(400);
-  expect(res.body.message).toBe("name, email, and password are required");
+test("updateUser", async () => {
+  const updatedUser = { email: "updatedEmail@email.com", password: "newPassword" };
+  const updateUserRes = await request(app)
+    .put(`/api/auth/${testUserId}`)
+    .set("Authorization", `Bearer ${testUserAuthToken}`)
+    .send(updatedUser);
+  expect(updateUserRes.status).toBe(200);
+
+  const { password, ...updatedUserWithoutPassword } = updatedUser;
+  expect(updateUserRes.body).toMatchObject(updatedUserWithoutPassword);
 });
