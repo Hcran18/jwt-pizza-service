7b493133e70bfffe2a119bfa61ee66745629407e
Hcran18 <hunter.crandall18@gmail.com>
2024-09-20 19:31:37 -0600

    test(add item to menu test)

---
+++ src/routes/orderRouter.test.js
@@ -0,0 +1,41 @@
+const request = require("supertest");
+const app = require("../service");
+const { DB, Role } = require("../database/database.js");
+
+function randomName() {
+  return Math.random().toString(36).substring(2, 12);
+}
+
+async function createAdminUser() {
+  let user = { password: "toomanysecrets", roles: [{ role: Role.Admin }] };
+  user.name = randomName();
+  user.email = user.name + "@admin.com";
+
+  await DB.addUser(user);
+
+  user.password = "toomanysecrets";
+  return user;
+}
+
+async function login(user) {
+  const res = await request(app).put("/api/auth").send(user);
+  console.log(res.body);
+  return res.body.token;
+}
+
+test("add item to menu", async () => {
+  const adminUser = await createAdminUser();
+  const adminToken = await login(adminUser);
+
+  const item = {
+    title: "Student",
+    description: "No topping, no sauce, just carbs",
+    image: "pizza9.png",
+    price: 0.0001,
+  };
+  const res = await request(app)
+    .put("/api/order/menu")
+    .set("Authorization", `Bearer ${adminToken}`)
+    .send(item);
+  expect(res.status).toBe(200);
+});
