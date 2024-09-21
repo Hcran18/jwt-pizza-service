df2bd8a6f2c0025bf3ab9c46debae087d5bf70ba
Hcran18 <hunter.crandall18@gmail.com>
2024-09-21 15:18:51 -0600

    test(get user franchises)

--- src/routes/franchiseRouter.test.js
+++ src/routes/franchiseRouter.test.js
@@ -2,6 +2,10 @@ const request = require("supertest");
 const app = require("../service");
 const { DB, Role } = require("../database/database.js");
 
+let adminUser;
+let adminID;
+let token;
+
 function randomName() {
   return Math.random().toString(36).substring(2, 12);
 }
@@ -19,6 +23,7 @@ async function createAdminUser() {
 
 async function login(user) {
   const res = await request(app).put("/api/auth").send(user);
+  adminID = res.body.user.id;
   return res.body.token;
 }
 
@@ -41,4 +46,17 @@ test("get all franchises", async () => {
   expect(res.status).toBe(200);
   expect(res.body.length).toBeGreaterThan(0);
   expect(res.body.some((franchise) => franchise.id === franchiseID)).toBe(true);
+});
+
+test("get user franchises", async () => {
+  const franchiseID = await createFranchise();
+  const res = await request(app)
+    .get(`/api/franchise/${adminID}`)
+    .set("Authorization", `Bearer ${token}`);
+  expect(res.status).toBe(200);
+  for (const franchise of res.body) {
+    for (const admin of franchise.admins) {
+      expect(admin.id).toBe(adminID);
+    }
+  }
 });
